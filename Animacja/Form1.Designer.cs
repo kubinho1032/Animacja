7eba4d0c9ae5002f0b68c3bde55e5aab29c30240
Jakub Korczyński <kubinho1032@wp.pl>
2017-01-30 15:38:31 +0100

    Add files via upload

---
+++ Animacja/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Animacja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.animacjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.napisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.koniecToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.animacjaToolStripMenuItem,
+            this.koniecToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(632, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // animacjaToolStripMenuItem
+            // 
+            this.animacjaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.napisToolStripMenuItem});
+            this.animacjaToolStripMenuItem.Name = "animacjaToolStripMenuItem";
+            this.animacjaToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.animacjaToolStripMenuItem.Text = "Animacja";
+            // 
+            // napisToolStripMenuItem
+            // 
+            this.napisToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem});
+            this.napisToolStripMenuItem.Name = "napisToolStripMenuItem";
+            this.napisToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.napisToolStripMenuItem.Text = "Uruchom";
+            this.napisToolStripMenuItem.Click += new System.EventHandler(this.napisToolStripMenuItem_Click);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // koniecToolStripMenuItem
+            // 
+            this.koniecToolStripMenuItem.Name = "koniecToolStripMenuItem";
+            this.koniecToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.koniecToolStripMenuItem.Text = "Koniec";
+            this.koniecToolStripMenuItem.Click += new System.EventHandler(this.koniecToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 453);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem animacjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem napisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem koniecToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Animacja/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Animacja
+{
+    public partial class Form1 : Form
+    {
+        private int x = 40;
+        private int y = 40;
+        private int z1 = 1;
+        private int z2 = 0;
+        private int h1,h2;
+        private int s_1_x=50;
+        private int s_1_y = 260;
+        private int s_2_x = 400;
+        private int s_2_y = 150;
+        Bitmap tlo, ludzik, saml, samp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            h1 = this.Width-50;
+            h2 = this.Width - 50;
+            tlo = new Bitmap("tlo.bmp");
+            ludzik = new Bitmap("ludzik.bmp");
+            saml = new Bitmap("saml.bmp");
+            samp = new Bitmap("samp.bmp");
+            this.DoubleBuffered =true;  
+            //ludzik.MakeTransparent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g=e.Graphics;
+            g.DrawImage(ludzik, 50, 300);
+            g.DrawImage(tlo, 0, 0);
+            g.DrawImage(saml, s_1_x, s_1_y);
+            g.DrawImage(samp, s_2_x, s_2_y);
+;
+        }
+
+        private void napisToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+         // samochod 1   
+            if (s_1_x >= h1)
+            {
+                z1=0;
+            }
+           
+            if (s_1_x <= 0) { z1 = 1; }
+
+            if (z1 == 1 && s_1_x <= h1)
+            {
+                s_1_x += 5;
+            }
+            if (z1 == 0 && s_1_x >= 0)
+            {
+                s_1_x -= 5;
+            }
+
+         // samochod 2 
+            if (s_2_x >= h2)
+            {
+                z2 = 0;
+            }
+
+            if (s_2_x <= 0) { z2 = 1; }
+
+            if (z2 == 1 && s_2_x <= h2)
+            {
+                s_2_x += 5;
+            }
+            if (z2 == 0 && s_2_x >= 0)
+            {
+                s_2_x -= 5;
+            }
+
+            Invalidate();
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void koniecToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
